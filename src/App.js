f77041d96e0ecdbd30f2342f486b16dd2c15c505
Jonatas Siqueira Lopes <jonatas.siq.lopes@gmail.com>
2021-09-08 12:13:16 -0400

    consertando funcao DELETE

--- src/App.js
+++ src/App.js
@@ -21,8 +21,8 @@ function App() {
 
 
   const [allList, setAllList] = useState([]);
-  const [id, setId] = useState("");
-  const [roiValue, setRoiValue] = useState(0);
+  //const [id, setId] = useState(0);
+  //const [roiValue, setRoiValue] = useState(0);
 
   //funçoes para o Banco de Dados, mostrar, lista, adicionar, editar, deletar e calcular Roi
   const showList = () => {
@@ -30,57 +30,92 @@ function App() {
   };
 
   const addProj = () => {
-    Axios.post("http://localhost:3001/create", { formNome: formNome, formDataInicio: formDataInicio, formDataFim: formDataFim, formValor: formValor, formRisco: formRisco, formMembros: formMembros, }).then(() => { setAllList([...allList, { id: id, formNome: formNome, formDataInicio: formDataInicio, formDataFim: formDataFim, formValor: formValor, formRisco: formRisco, formMembros: formMembros, }]); alert("Inserido no banco de Dados") })
+    Axios.post("http://localhost:3001/create", { formNome: formNome, formDataInicio: formDataInicio, formDataFim: formDataFim, formValor: formValor, formRisco: formRisco, formMembros: formMembros, }).then(() => { setAllList([...allList, { formNome: formNome, formDataInicio: formDataInicio, formDataFim: formDataFim, formValor: formValor, formRisco: formRisco, formMembros: formMembros, }]); alert("Inserido no banco de Dados. Aperte F5.") })
   };
   const editProj = () => {
-    Axios.put("http://localhost:3001/editar", { id: id, formNome: newFormNome, formDataInicio: newFormDataInicio, formDataFim: newFormDataFim, formValor: newFormValor, formRisco: newFormRisco, formMembros: newFormMembros, }).then(() => { setAllList([...allList, { id: id, formNome: newFormNome, formDataInicio: newFormDataInicio, formDataFim: newFormDataFim, formValor: newFormValor, formRisco: newFormRisco, formMembros: newFormMembros, }]); alert("Banco de Dados Atualizado") })
-  };
-  const delProj = () => {
-    //GET numId
-    //select ROW*
-    //confirm ? del : home
-  };
-  const calcProj = () => {
-    //GET numId,
-    //select (valor, risco),
-    //if FormValor>valorProjDB ? SWITCH risco case1(risco=0 return 5%); 
-    //alert (o valor digitado 'e menor do que valor do proj) break;
+    Axios.put("http://localhost:3001/editar", { formNome: newFormNome, formDataInicio: newFormDataInicio, formDataFim: newFormDataFim, formValor: newFormValor, formRisco: newFormRisco, formMembros: newFormMembros, }).then(() => { setAllList([...allList, { id: id, formNome: newFormNome, formDataInicio: newFormDataInicio, formDataFim: newFormDataFim, formValor: newFormValor, formRisco: newFormRisco, formMembros: newFormMembros, }]); alert("Banco de Dados Atualizado. Aperte F5.") })
   };
 
+  // const delProj = (idProj) => {
+  // //var del = window.confirm("Deseja realmente deletar o Projeto na Base de Dados?");
+  // // if (del === true) {
+  // Axios.delete(`http://localhost:3001/delete/${idProj}`); //.then((response) => { setAllList(allList.filter((val) => { return val.idProj === idProj })) });
+  // //  } else { console.log("fail"); };
+  // };
+
+  // const calcProj = (idProj) => {
+  // Axios.get("http://localhost:3001/calc", { id: id, roiValue: roiValue, }).then((response) => { setAllList(response.data); })
+  // function CalcRoiProj() {
+  // var valorDB = Axios.get("http://localhost:3001/roi", { id: id, formValor: formValor }).then((response) => { setAllList(response.data); console.log(valorDB) });
+  // var riscoDB = Axios.get("http://localhost:3001/list", { id: id, formRisco: formRisco }).then((response) => { setAllList(response.data){ val.riscoProj }; });
+  // var formValor = document.querySelector('#idSelected');
+  // var novoValor = formValor;
+  // if (formValor > valorDB) {
+  //   switch (riscoDB) {
+  //     case 0:
+  //       novoValor += (novoValor * 5) / 100;
+  //       novoValor.toFixed(3);
+  //       console.log("O investimento de " + formValor + " reais tem retorno de " + novoValor + " reais.");
+  //       break;
+  //     case 1:
+  //       novoValor += (novoValor * 10) / 100;
+  //       novoValor.toFixed(2);
+  //       console.log("O investimento de " + formValor + " reais tem retorno de " + novoValor + " reais.");
+  //       break;
+  //     case 2:
+  //       novoValor += (novoValor * 20) / 100;
+  //       novoValor.toFixed(2);
+  //       console.log("O investimento de " + formValor + " reais tem retorno de " + novoValor + " reais.");
+  //       break;
+  //     default:
+  //       document.write("Desculpe, entrada desconhecida");
+  //   }
+  // } else {
+  //   alert("Investimento mencionado menor que o valor do Projeto");
+  // };
+  // };
+
+
+  //CalcRoiProj();
+  //select (valor, risco),
+  //if FormValor>valorProjDB ? SWITCH risco case1(risco=0 return 5%); 
+  //alert (o valor digitado 'e menor do que valor do proj) break;
+  // };
+
   //gerenciar os elementos de visualizaçao
-  const displayHidden2 = () => {
-    console.log("ok");
-  };
+  // const displayHidden2 = () => {
+  //   console.log("ok");
+  // };
 
   //COMEÇO DO APLICATIVO
   return (<div className="App">
     <div className='mainDiv'><header><p id='logo'>Loren Ipsum Inc.</p></header></div>
 
+    <div className='mainDiv'><div className="divBtn"><button onClick={showList} id='showListBtn'>Ver Lista</button></div>
+    </div>
+
     <div className='mainDiv'>{allList.map((val, key) => {
       return <div className='listaProj'>
+
         <h3>ID: {val.idProj}</h3>
         <h3>Nome: {val.nomeProj}</h3>
         <h3>Data começo: {val.dataInicioProj}</h3>
         <h3>Data final: {val.dataFimProj}</h3>
         <h3>Valor do Projeto: {val.valorProj}</h3>
         <h3>Indice de risco: {val.riscoProj}</h3>
-        <h3>Participantes: {val.membrosProj}</h3> </div>
+        <h3>Participantes: {val.membrosProj}</h3>
+        <div>
+          {" "}
+          <button onClick={addProj} id='simularBtn1'>Simular</button>
+          {/* <button onClick={delProj(val.idProj)} id='excluirBtn'>Excluir</button> */}
+        </div></div>
     })}</div>
 
-    <div className='mainDiv'><div className="divBtn"><button onClick={showList} id='showListBtn'>Ver Lista</button></div>
-    </div>
-
-    <div className='mainDiv'><div className='hideit1'><label>Digite ID do Projeto</label><input id="idSelected" type="number" onChange={(event) => { setId(event.target.value); }} required></input>
-      <div className='divBtn'>
-        <button id='excluirBtn'>Excluir</button>
-        <button id='simularBtn'>Simular</button>
-      </div>
-    </div></div>
-
-    <div className='mainDiv'><div className='hideit2'><label> - Simular Retorno de Investimento - </label>
+    {/* <div className='mainDiv'><div className='hideit2'><label> - Simular Retorno de Investimento - </label>
+      <label>Digite ID do Projeto</label><input id="idSelected" type="number" onChange={(event) => { setId(event.target.value); }} required></input>
       <label>Valor do Investimento</label><input name="formInvest" type="number" step=".01" onChange={(event) => { setRoiValue(event.target.value); }} required></input>
       <div className='divBtn'><button onClick={calcProj} id='simularBtn1'>Simular</button>
-      </div></div></div>
+      </div></div></div> */}
 
     <div className='mainDiv'>
       <div className='hideit3'><label> - Informações do Projeto - </label>
@@ -98,7 +133,7 @@ function App() {
     </div>
 
     <div className='mainDiv'>
-      <div className='hideit4'><label> - Informações do Projeto - </label>
+      <div className='hideit4'><label> - Editar Projeto - </label>
         <p><i>Todos os campos são obrigatórios*</i></p>
         <label>ID do Projeto*</label> <input id="formID" type="number" onChange={(event) => { setId(event.target.value); }}></input>
         <label>Nome do Projeto*</label> <input id="newFormNome" type="text" onChange={(event) => { setNewFormNome(event.target.value); }} required></input>
@@ -108,7 +143,6 @@ function App() {
         <label>Nível de Risco* (0=baixo; 1=médio; 2=alto) </label> <input name="newFormRisco" type="number" min="0" max="2" onChange={(event) => { setNewFormRisco(event.target.value); }} required></input>
         <label>Membros do Projeto*</label> <input name="newFormMembros" type="text" onChange={(event) => { setNewFormMembros(event.target.value); }} required></input>
         <div className="divBtn">
-          <button onClick={addProj} id='cadastrarBtn'>Cadastrar</button>
           <button onClick={editProj} id='editarBtn'>Editar</button>
         </div>
       </div>
